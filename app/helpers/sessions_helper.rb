ff535b7e6a682c33db16771d4db1cd65b287279a
Tetsuji Hayakawa <majoclub@gmail.com>
2019-04-27 10:48:13 +0900

    Implement basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,4 +16,11 @@ module SessionsHelper
   def logged_in?
     !current_user.nil? 
   end
+
+  # 現在のユーザをログアウトする
+  def log_out
+    #forget(current_user)
+    session.delete(:user_id) 
+    @current_user = nil
+  end
 end
