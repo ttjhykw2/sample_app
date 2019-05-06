e701d965ec7c5d6378b1eb26356863263ba6f8de
Tetsuji Hayakawa <majoclub@gmail.com>
2019-05-06 14:34:30 +0900

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                   foreign_key: "follower_id",
+                   dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+           foreign_key: "followed_id",
+           dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   # before_save { self.email = email.downcase }
   # before_save { email.downcase! }
@@ -88,7 +96,26 @@ class User < ApplicationRecord
   # 試作feedの定義
   # 完全な実装はまだ
   def feed
-    Micropost.where("user_id = ?", id)
+    #Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+
+    following_ids = "SELECT followed_id FROM relationships
+                      WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+
+  # ユーザをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザがフォローしていたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
