308b436acd30905e99fc751d22b50db6b69f0779
Tetsuji Hayakawa <majoclub@gmail.com>
2019-11-04 22:53:14 +0900

    add rspec for controller static_pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-  before do
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
+  # before do
+  #  @base_title = "Ruby on Rails Tutorial Sample App"
+  # end
 
   it "should get root" do
     get :home
     expect(response).to have_http_status :success
   end
-=begin
+
   it "should get home" do
     get :home
     expect(response).to have_http_status :success
@@ -17,21 +17,20 @@ RSpec.describe StaticPagesController, type: :controller do
   end
 
   it "should get help" do
-    get help_path
+    get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    #assert_select "title", "Help | #{@base_title}"
   end
 
   it "should get about" do
-    get about_path
+    get :about
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    #assert_select "title", "About | #{@base_title}"
   end
 
   it "should get contact" do
-    get contact_path
+    get :contact
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    #assert_select "title", "Contact | #{@base_title}"
   end
-=end
 end
